d78fcb05a4998604a585d710462f251897efef01
iracopter <irka.kokarewa@gmail.com>
2023-10-24 16:25:35 +0300

    controlvalueaccessor added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { isPasswordStrong } from '../services/password-strength-checker';
 import { updatePasswordStrength } from '../services/password-strength-updater';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: AppComponent,
+      multi: true,
+    },
+  ],
 })
-export class AppComponent {
+export class AppComponent implements ControlValueAccessor {
   inputData: string = '';
   shouldGray: boolean = true;
 
@@ -16,8 +24,25 @@ export class AppComponent {
   isPasswordMedium: boolean = false;
   isPasswordStrong: boolean = false;
 
+  private onChange: (value: string) => void = () => {};
+  private onTouched: () => void = () => {};
+
+  writeValue(value: any): void {
+    this.inputData = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
   handleInputChange(value: string) {
     const passwordStrength = isPasswordStrong(value);
-    updatePasswordStrength(this, passwordStrength);    
+    updatePasswordStrength(this, passwordStrength);
+    this.onChange(value); 
+    this.onTouched(); 
   }
 }
